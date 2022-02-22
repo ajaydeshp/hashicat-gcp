58741118d07c13f8865987a29b0aec77b1d2daa2
ajaydeshp <62387118+ajaydeshp@users.noreply.github.com>
2022-02-22 14:46:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,9 +47,7 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  labels 
-  { "department" = "devops",
-  "billable" = "true" }
+ 
   
 
   boot_disk {
